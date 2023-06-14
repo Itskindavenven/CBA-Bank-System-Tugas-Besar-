a8333dc3209fd3bd93ff4b5cc9c52e308a796919
Arif Suryo Kurniawan <108004317+zyrelll@users.noreply.github.com>
2023-06-14 21:55:52 +0700

    Update TableBO.java

--- src/table/TableBO.java
+++ src/table/TableBO.java
@@ -3,7 +3,7 @@ import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import model.Back_Office;
 
-public class TableBO {
+public class TableBO extends AbstractTableModel{
     private List<Back_Office> list;
     
     public TableBO(List<Back_Office> list){
@@ -32,6 +32,23 @@ public class TableBO {
                 return list.get(rowIndex).getGaji();
             case 10:
                 return list.get(rowIndex).getPassword();
+            default:
+                return null;
+        }
+    }
+    
+    public String getColumnName(int column){
+        switch(column){
+            case 0:
+                return "ID";
+            case 1:
+                return "Nama";
+            case 2:
+                return "Username";
+            case 3:
+                return "Tanggal Lahir";
+            case 4:
+                return "Gaji";
             default:
                 return null;
         }
