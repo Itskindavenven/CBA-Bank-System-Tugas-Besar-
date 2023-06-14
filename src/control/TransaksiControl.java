0673194521fcb9695edb305e8b90744453f61e5b
Axellio Rintoko <83566850+Exellll@users.noreply.github.com>
2023-06-14 20:22:12 +0700

    Update TransaksiControl.java

--- src/control/TransaksiControl.java
+++ src/control/TransaksiControl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import dao.TransaksiDAO;
 import java.sql.SQLException;
 import model.Transaksi;
+import table.TableTransaksi;
 
 public class TransaksiControl {
     private TransaksiDAO tDAO = new TransaksiDAO();
@@ -15,5 +16,19 @@ public class TransaksiControl {
     
     public void insertTransaksi(Transaksi transaksi) throws SQLException{
         tDAO.insertTransaksi(transaksi);
+    }
+    
+    public TableTransaksi showTransaksi(String query){
+        List<Transaksi> dataTransaksi = tDAO.showTransaksi(query);
+        TableTransaksi tableTransaksi = new TableTransaksi(dataTransaksi);
+        
+        return tableTransaksi;
+    }
+    
+    public TableTransaksi showTransaksiBy(String query){
+        List<Transaksi> dataTransaksi = tDAO.showTransaksiBy(query);
+        TableTransaksi tableTransaksi = new TableTransaksi(dataTransaksi);
+        
+        return tableTransaksi;
     }
 }
