8b3ecbc250c860161d52681ec85e20130d434be0
Axellio Rintoko <83566850+Exellll@users.noreply.github.com>
2023-06-14 20:22:38 +0700

    Update TransaksiDAO.java

--- src/dao/TransaksiDAO.java
+++ src/dao/TransaksiDAO.java
@@ -9,6 +9,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.math.BigInteger;
 
 public class TransaksiDAO {
     private final DbConnection dbConnection = new DbConnection();
@@ -64,6 +67,80 @@ public class TransaksiDAO {
         }finally{
             dbConnection.closeConnection();
         }
+    }
+    
+    public List<Transaksi> showTransaksiBy(String query){
+        Connection connection = dbConnection.makeConnection();
+        
+        String sql = "SELECT * FROM transaksi WHERE id LIKE '%" + query + "%' OR no_rekening LIKE '%" + query + "%' ORDER BY tanggal";
+        
+        System.out.println("Mengambil data Transaksi...");
+        
+        List<Transaksi> list = new ArrayList();
+        
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if(rs != null){
+                while(rs.next()){
+                    Transaksi t = new Transaksi(
+                            rs.getString("id"),
+                            rs.getString("jenis"),
+                            rs.getString("tanggal"),
+                            new BigInteger(rs.getString("jumlah")),
+                            rs.getString("no_rekening")
+                    );
+                    list.add(t);
+                }
+            }
+            rs.close();
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error reading database...");
+            System.out.println(e);
+        }
+        dbConnection.closeConnection();
+        
+        return list;
+        
+    }
+    
+    
+    public List<Transaksi> showTransaksi(String query){
+        Connection connection = dbConnection.makeConnection();
+        
+        String sql = "SELECT * FROM transaksi";
+        System.out.println("Mengambil data Transaksi...");
+        
+        List<Transaksi> list = new ArrayList();
+        
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if(rs != null){
+                while(rs.next()){
+                    Transaksi t = new Transaksi(
+                            rs.getString("id"),
+                            rs.getString("jenis"),
+                            rs.getString("tanggal"),
+                            new BigInteger(rs.getString("jumlah")),
+                            rs.getString("no_rekening")
+                    );
+                    list.add(t);
+                }
+            }
+            rs.close();
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error reading database...");
+            System.out.println(e);
+        }
+        dbConnection.closeConnection();
+        
+        return list;
+        
     }
 
 }
